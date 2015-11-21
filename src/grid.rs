ba967b3949e266da79597256a152270a3d88ae2b
Steve Wooster <s.f.m.wooster+github@gmail.com>
2015-11-20 16:32:49 -0800

    Added rusudoku::grid::Grid
    This'll be used to track puzzles/pencilmarks/etc. I'll probably need to
    add more stuff, such as better indexing, but this is a start.
    
    Note: This also added equality-testing to CellId/ValueId/CaseId to assist
    with testing.

--- src/grid.rs
+++ src/grid.rs
@@ -1,21 +1,25 @@
 //! Data structures for tracking cell/value possibilities.
 
+use std::iter;
+use std::ops;
+use std::slice;
+
 /// Cell value index.
 ///
 /// Represents a possible value of a cell, typically 1-9.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct ValueId(usize);
 
 /// Cell position index.
 ///
 /// This is used to address a particular cell on a grid.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct CellId(usize);
 
 /// Cell/value combination index.
 ///
 /// This is used to address the possibility that a particular cell is a particular value.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct CaseId(usize);
 
 /// Marker trait to indicate grid-size-dependent conversions.
@@ -141,8 +145,185 @@ impl IndexConverter for SimpleConverter {
     fn conversion_size(&self) -> usize { self.0 }
 }
 
-/// Not yet implemented.
-pub struct Grid;
+/// Stores the possible values for each cell of a puzzle.
+///
+/// A grid contains cells arranged in a square (with `size` cells per side). Each cell
+/// has `size` different `bool`s, each one indicating whether the cell may be a particular
+/// value.
+pub struct Grid {
+    size: usize,
+    cases: Vec<bool>, // TODO: Memory-efficient implementation?
+}
+
+impl Grid {
+    /// Creates a new `Grid` for a puzzle that is `size` cells wide.
+    pub fn new(size: usize) -> Grid {
+        Grid {
+            size: size,
+            cases: vec![true; size * size * size]
+        }
+    }
+
+    /// Returns the width of the grid in cells.
+    pub fn size(&self) -> usize { self.size }
+
+    /// Returns iterator over all cell positions/values.
+    ///
+    /// Iterates over all cells in row-major order, yielding items of type `(CellId, &[bool])`,
+    /// representing a cell's position, whether it can be each value.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use rusudoku::grid::*;
+    /// let grid = Grid::new(9);
+    /// let (cell, values) = grid.cells().nth(5).unwrap();
+    /// assert_eq!(cell, grid.convert((5, 0)));
+    /// assert!(values[0]); // The cell could be the value 1.
+    /// ```
+    pub fn cells(&self) -> CellsIter {
+        let cell_id: fn(_) -> _ = CellId;
+        (0..).map(cell_id).zip(self.cases.chunks(self.size))
+    }
+
+    /// Returns mutable iterator over all cell positions/values.
+    ///
+    /// Identical to `cells()`, except that the boolean array references are mutable.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use rusudoku::grid::*;
+    /// let mut grid = Grid::new(9);
+    /// let (cell, values) = grid.cells_mut().nth(5).unwrap();
+    /// values[0] = false; // Prevent the cell from being the value 1.
+    /// ```
+    pub fn cells_mut(&mut self) -> CellsIterMut {
+        let cell_id: fn(_) -> _ = CellId;
+        (0..).map(cell_id).zip(self.cases.chunks_mut(self.size))
+    }
+
+    fn cell_range(&self, cell_id: CellId) -> ops::Range<usize> {
+        let size = self.size;
+        cell_id.0 * size .. cell_id.0 * size + size
+    }
+
+    /// Returns iterator over cases/values of a cell.
+    ///
+    /// Given a CellId (or something that can be converted to one), returns an iterator over the
+    /// cases that make up a particular cell, yielding items of type `(CaseId, &bool)`,
+    /// representing a particular case (i.e. position and value) and whether or not that case is
+    /// possible.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use rusudoku::grid::*;
+    /// let mut grid = Grid::new(9);
+    /// let (case, value) = grid.cell((5, 0)).nth(3).unwrap();
+    /// assert!(*value); // The cell at 5,0 could be the value 4.
+    /// ```
+    pub fn cell<T>(&self, cell_id: T) -> CasesIter
+        where CellId: FromIndex<T> {
+        let case_id: fn(_) -> _ = CaseId;
+        let range = self.cell_range(self.convert(cell_id));
+        range.clone().map(case_id).zip(self.cases[range].iter())
+    }
+
+    /// Returns mutable iterator over cases/values of a cell.
+    ///
+    /// Identical to `cell(...)`, except that the boolean references are mutable.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use rusudoku::grid::*;
+    /// let mut grid = Grid::new(9);
+    /// let (case, value) = grid.cell_mut((5, 0)).nth(3).unwrap();
+    /// *value = false; // Prevent the cell at 5,0 from being the value 4.
+    /// ```
+    pub fn cell_mut<T>(&mut self, cell_id: T) -> CasesIterMut
+        where CellId: FromIndex<T> {
+        let case_id: fn(_) -> _ = CaseId;
+        let range = self.cell_range(self.convert(cell_id));
+        range.clone().map(case_id).zip(self.cases[range].iter_mut())
+    }
+
+    /// Returns iterator over all cases/values.
+    ///
+    /// Iterates over the cases of the puzzle, yielding items of type `(CaseId, &bool)`,
+    /// representing a particular case (i.e. position and value) and whether or not that
+    /// case is possible.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use rusudoku::grid::*;
+    /// let mut grid = Grid::new(9);
+    /// let (case, value) = grid.cases().nth(100).unwrap();
+    /// assert!(*value); // The cell at 2,1 could be the value 2.
+    /// ```
+    pub fn cases(&self) -> CasesIter {
+        let case_id: fn(_) -> _ = CaseId;
+        (0..self.cases.len()).map(case_id).zip(self.cases.iter())
+    }
+
+    /// Returns mutable iterator over all cases/values.
+    ///
+    /// Identical to `cases()`, except that the boolean references are mutable.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use rusudoku::grid::*;
+    /// let mut grid = Grid::new(9);
+    /// let (case, value) = grid.cases_mut().nth(100).unwrap();
+    /// *value = false; // Prevent the cell at 2,1 from being the value 2.
+    /// ```
+    pub fn cases_mut(&mut self) -> CasesIterMut {
+        let case_id: fn(_) -> _ = CaseId;
+        (0..self.cases.len()).map(case_id).zip(self.cases.iter_mut())
+    }
+}
+
+pub type CellsIter<'a>    = iter::Zip<iter::Map<ops::RangeFrom<usize>, fn(usize) -> CellId>, slice::Chunks<'a, bool>>;
+pub type CellsIterMut<'a> = iter::Zip<iter::Map<ops::RangeFrom<usize>, fn(usize) -> CellId>, slice::ChunksMut<'a, bool>>;
+pub type CasesIter<'a>    = iter::Zip<iter::Map<ops::Range<usize>,     fn(usize) -> CaseId>, slice::Iter<'a, bool>>;
+pub type CasesIterMut<'a> = iter::Zip<iter::Map<ops::Range<usize>,     fn(usize) -> CaseId>, slice::IterMut<'a, bool>>;
+
+impl IndexConverter for Grid {
+    fn conversion_size(&self) -> usize { self.size }
+}
+
+impl ops::Index<CellId> for Grid {
+    type Output = [bool];
+    fn index(&self, cell_id: CellId) -> &[bool] {
+        let range = self.cell_range(cell_id);
+        &self.cases[range]
+    }
+}
+
+impl ops::IndexMut<CellId> for Grid {
+    fn index_mut(&mut self, cell_id: CellId) -> &mut [bool] {
+        let range = self.cell_range(cell_id);
+        &mut self.cases[range]
+    }
+}
+
+impl ops::Index<CaseId> for Grid {
+    type Output = bool;
+    fn index(&self, CaseId(case_id): CaseId) -> &bool {
+        &self.cases[case_id]
+    }
+}
+
+impl ops::IndexMut<CaseId> for Grid {
+    fn index_mut(&mut self, CaseId(case_id): CaseId) -> &mut bool {
+        &mut self.cases[case_id]
+    }
+}
+
+// Wish I could figure out how to get Index<FromIndex<T>> to work...
 
 #[cfg(test)]
 mod tests {
@@ -235,5 +416,127 @@ mod tests {
         assert_eq!(value_id, 8);
         let CaseId(case_id) = c.convert((CellId(cell_id), ValueId(value_id)));
         assert_eq!(case_id, 3160);
+    }
+
+    #[test]
+    fn test_new_grid() {
+        let grid = Grid::new(9);
+        assert_eq!(grid.size(), 9);
+        assert_eq!(grid.cases.len(), 729);
+    }
+
+    #[test]
+    fn test_grid_cells_iter() {
+        let mut grid = Grid::new(9);
+        for i in vec![9,10,12,13,14,16,17] {
+            grid.cases[i] = false;
+        }
+        assert_eq!(grid.cells().count(), 81);
+        let cells: Vec<_> = grid.cells().take(3).collect();
+        assert_eq!(cells[0].0, grid.convert((0,0)));
+        assert_eq!(cells[1].0, grid.convert((1,0)));
+        assert_eq!(cells[2].0, grid.convert((2,0)));
+        assert_eq!(cells[0].1, [true; 9]);
+        assert_eq!(cells[1].1, [false, false, true, false, false, false, true, false, false]);
+        assert_eq!(cells[2].1, [true; 9]);
+    }
+
+    #[test]
+    fn test_grid_cells_mut_iter() {
+        let mut grid = Grid::new(9);
+        let cell = {
+            let (cell, possibilities) = grid.cells_mut().nth(3).unwrap();
+            possibilities[0] = false;
+            possibilities[3] = false;
+            possibilities[8] = false;
+            cell
+        };
+        assert_eq!((3,0), grid.convert(cell));
+        assert!(grid.cases[27..36] == [false, true, true, false, true, true, true, true, false]);
+    }
+
+    #[test]
+    fn test_grid_cell_iter() {
+        let mut grid = Grid::new(9);
+        for i in vec![9,10,12,13,14,16,17] {
+            grid.cases[i] = false;
+        }
+        let (cases, possibilities): (Vec<_>, Vec<_>) = grid.cell((1,0)).map(|(case, &ok)| (case, ok)).unzip();
+        assert_eq!(possibilities, [false, false, true, false, false, false, true, false, false]);
+        let (cells, values): (Vec<_>, Vec<_>) = cases.iter()
+                                                     .map(|&case| grid.convert::<_, (CellId, ValueId)>(case))
+                                                     .unzip();
+        assert!(cells.iter().all(|&cell| cell == grid.convert((1,0))));
+        assert_eq!(values[0], grid.convert(1));
+        assert_eq!(values[8], grid.convert(9));
+    }
+
+    #[test]
+    fn test_grid_cell_mut_iter() {
+        let mut grid = Grid::new(9);
+        let c = SimpleConverter(9);
+        for (i, (case, possibility)) in grid.cell_mut((1,0)).enumerate() {
+            let (cell, value): (CellId, ValueId) = c.convert(case);
+            assert_eq!((1,0), c.convert(cell));
+            assert_eq!(i+1, c.convert(value));
+            assert!(*possibility);
+            *possibility = false;
+        }
+        assert!(grid.cases[9..18] == [false; 9]);
+    }
+
+    #[test]
+    fn test_grid_cases_iter() {
+        let mut grid = Grid::new(9);
+        grid.cases[8] = false;
+        grid.cases[9] = false;
+        let cases: Vec<_> = grid.cases().skip(7).take(4).collect();
+        assert_eq!(cases[0].0, CaseId(7));
+        assert_eq!(cases[1].0, CaseId(8));
+        assert_eq!(cases[2].0, CaseId(9));
+        assert_eq!(cases[3].0, CaseId(10));
+        assert!(cases[0].1);
+        assert!(!cases[1].1);
+        assert!(!cases[2].1);
+        assert!(cases[3].1);
+    }
+
+    #[test]
+    fn test_grid_cases_mut_iter() {
+        let mut grid = Grid::new(9);
+        {
+            let (case, possibility) = grid.cases_mut().nth(10).unwrap();
+            *possibility = false;
+            assert_eq!(case, CaseId(10));
+        }
+        assert!(grid.cases[9]);
+        assert!(!grid.cases[10]);
+        assert!(grid.cases[11]);
+    }
+
+    #[test]
+    fn test_grid_index_by_cell() {
+        let mut grid = Grid::new(9);
+        for i in vec![0,1,3,4,5,7,8] {
+            grid[CellId(4)][i] = false;
+        }
+        assert!(grid[CellId(3)] == [true; 9]);
+        assert!(grid[CellId(4)] == [false, false, true, false, false, false, true, false, false]);
+        assert!(grid[CellId(5)] == [true; 9]);
+    }
+
+    #[test]
+    fn test_grid_index_by_case() {
+        let mut grid = Grid::new(9);
+        for i in vec![0,1,5,20,50,80] {
+            assert!(grid[CaseId(i)]);
+            grid[CaseId(i)] = false;
+            assert!(!grid[CaseId(i)]);
+        }
+        assert!(grid[CaseId(2)]);
+        assert!(grid[CaseId(6)]);
+        assert!(grid[CaseId(25)]);
+        assert!(grid[CaseId(40)]);
+        assert!(grid[CaseId(79)]);
     }
 }
